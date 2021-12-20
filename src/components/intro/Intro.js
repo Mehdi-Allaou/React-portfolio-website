ac156a6e1729b0a14414dc0e6284cc4bb3d407bc
Mehdi Allaou <mehdi_allaou@hotmail.com>
2021-12-20 23:16:32 +0100

    building up structure

---
+++ src/components/intro/Intro.js
@@ -0,0 +1,39 @@
+import './intro.scss'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
+
+export const Intro = () => {
+
+  const textRef = useRef()
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,
+      backspeed: 60,
+      showCursor: true,
+      strings: ['Developer', 'Designer']
+    });
+  }, [])
+
+  return (
+    <div className='intro' id='intro'>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Mehdi Allaou</h1>
+          <h3>Junior <span ref={textRef}></span></h3>
+        </div>
+        <a href="#portfolio">
+
+        <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/works/Works.js
@@ -0,0 +1,57 @@
+import './works.scss';
+import { useState } from 'react';
+
+export const Works = () => {
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const data = [
+    { id: '1', 
+      icon: './assets/mobile.png', 
+      title: 'Web Design',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. At ducimus fugit mollitia eligendi, sint illum?',
+      img: 'https://images.unsplash.com/photo-1638913971873-bcef634bdcd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+    },
+    { id: '2', 
+      icon: './assets/globe.png', 
+      title: 'Globe design',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. At ducimus fugit mollitia eligendi, sint illum?',
+      img: 'https://images.unsplash.com/photo-1639905517784-b036046b14fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+    }
+  ];
+
+  const handleClick = (way) => {
+    way === 'left' ? setCurrentSlide(currentSlide > 0 ? currentSlide -1 : 2) : 
+    setCurrentSlide(currentSlide<data.length -1 ? currentSlide + 1 : 0)
+  }
+
+  return (
+    <div className='works' id='works'>
+      <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)`}}>
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.icon} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>
+                    {d.desc}
+                  </p>
+                    <span>Projects</span>
+                </div>
+              </div>
+              <div className="right">
+                <img src="https://images.unsplash.com/photo-1638913971873-bcef634bdcd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
+              </div>
+            </div>
+        </div>
+          ))} 
+      </div>
+      <img src="assets/arrow.png" className='arrow left' alt="" onClick={() => handleClick('left')} />
+      <img src="assets/arrow.png" className='arrow right' alt="" onClick={() => handleClick('right')} />
+    </div>
+  )
+}
