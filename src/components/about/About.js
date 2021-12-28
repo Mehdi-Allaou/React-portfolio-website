9e8c6902b6b0d398f6db2fb50a840e9dab63c2e2
Mehdi Allaou <mehdi_allaou@hotmail.com>
2021-12-28 20:19:34 +0100

    adjusting test and cv

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,4 +1,8 @@
 import './about.scss';
+import { Text } from './text/Text';
+
+
+
 
 export const About = () => {
   return (
@@ -9,8 +13,15 @@ export const About = () => {
       </div>
       <div className="container">
         <div className="left-container">
-            <img src="assets/picture3.png" alt="" />      
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima voluptas dolor temporibus architecto, qui dolore! Eius, sunt. Repellendus atque officia placeat expedita ullam delectus, sunt earum sit, perferendis consequatur hic! Quia minus vero harum laboriosam dicta ad fugit! Vitae ab aspernatur temporibus earum magni cumque. Tenetur facere officiis iure aperiam officia! Sunt, deserunt? Ab quas, nobis molestias esse cumque ut eius ad quae illum minus corrupti adipisci! Assumenda eum debitis officia consectetur quas laudantium reprehenderit ex recusandae ipsa veritatis necessitatibus saepe illum cumque, in aspernatur accusantium facere provident quam! Quod quibusdam reprehenderit assumenda corrupti sed molestias nobis eum cumque pariatur!</p>
+          <img src="assets/picture3.png" alt="" />      
+          <p>
+            <Text />
+          </p>
+          <div className="header__buttons">
+            {/* <a href="MehdiAllaouCVny.pdf" title='' className="btn btn-outline">Download Resume */}
+            {/* </a> */}
+            
+          </div>
         </div>
         <div className="right-container">
           <div className='img-container'>
---
+++ src/components/about/text/Text.js
@@ -0,0 +1,27 @@
+
+import React from 'react'
+
+export const Text = () => {
+
+  const firstInfo = `
+  Jag är en driven
+  
+  frontend-entusiast med bakgrund inom måleribranschen. Jag har sedan ett år tillbaka valt att lägga penseln på hyllan för att uppfylla min dröm om att jobba som JavaScript-utvecklare.
+
+  Intresset för programmering och IT är något som har funnits med mig ända sedan jag var liten. Exempelvis hjälpte jag under högstadietiden kompisar med olika datorproblem, såsom omformatering och byte av komponenter – något som i förlängningen ledde till att jag fördjupade mitt intresse för datorer och deras funktion, både mjuk- och hårdvarumässigt. Jag har vid ett flertal tillfällen byggt datorer från grunden och då behövt lära mig Bios-systemet på Windows, genom trial and error, för att optimera funktionen. Den ständiga felsökningen det innebar, var något som fascinerade och triggade mig att fortsätta mitt lärande. Vidare har jag under årens gång breddat mitt intresse för datorer och programmering genom att hålla mig uppdaterad via Udemy, Codecademy och Stackoverflow.
+  
+  I dagsläget studerar jag på en IT-påbyggnadsutbildning via Lexicon med inriktning mot frontend, vilket är både roligt och lärorikt. En sak jag tycker är extra bra med utbildningen är att det även ingår en del backend, exempelvis databaser och Node.js, vilket innebär att jag känner mig trygg med även de bitarna. Utbildningen innehåller även ramverk som Vue och React.
+  
+  I egenskap av före detta målare så har jag med mig kvaliteter som känsla för färgsättning och estetik, förmåga att möta och samarbeta med många olika typer av människor. Jag har även stor vana av att planera och genomföra mitt arbete på ett hållbart och effektivt sätt. Dessutom har jag haft möjligheten att leda projekt under min tidigare karriär och har tagit till mig ovärderlig kunskap om att kunna samspela med andra yrkesgrupper och se utmaningar ur deras perspektiv.
+  
+  Parallellt med utbildningen så bygger jag upp min portfolio med lite av mina sidoprojekt som just nu är under konstruktion och återfinns på GitHub (se länk under kontaktinfo).
+  
+  Då jag tar examen den 19 januari 2022 söker jag just nu efter en junior utvecklartjänst eller praktikplats / LIA med start efter examen och ser fram emot att få använda mina färdigheter ute i arbetslivet.
+  `;
+  return (
+    <>
+      {firstInfo}
+      
+    </>
+  )
+}
