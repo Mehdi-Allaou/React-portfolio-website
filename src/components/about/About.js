1e89858ae4cb5741d8a878ff3e449bc33cb6f613
Mehdi Allaou <mehdi_allaou@hotmail.com>
2021-12-28 16:08:46 +0100

    adjusting responsivity to all devices

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,10 +9,12 @@ export const About = () => {
       </div>
       <div className="container">
         <div className="left-container">
-          <p>Hello</p>
+            <img src="assets/picture3.png" alt="" />      
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima voluptas dolor temporibus architecto, qui dolore! Eius, sunt. Repellendus atque officia placeat expedita ullam delectus, sunt earum sit, perferendis consequatur hic! Quia minus vero harum laboriosam dicta ad fugit! Vitae ab aspernatur temporibus earum magni cumque. Tenetur facere officiis iure aperiam officia! Sunt, deserunt? Ab quas, nobis molestias esse cumque ut eius ad quae illum minus corrupti adipisci! Assumenda eum debitis officia consectetur quas laudantium reprehenderit ex recusandae ipsa veritatis necessitatibus saepe illum cumque, in aspernatur accusantium facere provident quam! Quod quibusdam reprehenderit assumenda corrupti sed molestias nobis eum cumque pariatur!</p>
         </div>
         <div className="right-container">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima voluptas dolor temporibus architecto, qui dolore! Eius, sunt. Repellendus atque officia placeat expedita ullam delectus, sunt earum sit, perferendis consequatur hic! Quia minus vero harum laboriosam dicta ad fugit! Vitae ab aspernatur temporibus earum magni cumque. Tenetur facere officiis iure aperiam officia! Sunt, deserunt? Ab quas, nobis molestias esse cumque ut eius ad quae illum minus corrupti adipisci! Assumenda eum debitis officia consectetur quas laudantium reprehenderit ex recusandae ipsa veritatis necessitatibus saepe illum cumque, in aspernatur accusantium facere provident quam! Quod quibusdam reprehenderit assumenda corrupti sed molestias nobis eum cumque pariatur!</p>
+          <div className='img-container'>
+          </div>
         </div>
       </div>
     </div>
